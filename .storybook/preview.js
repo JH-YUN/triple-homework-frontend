f1868a9d63dca854a978165a1aa4f06488a10629
junhan <wnsgks789@gmail.com>
2022-06-26 18:26:16 +0900

    chore: storybook 글로벌 스타일 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,16 @@
+import Global from '../src/style/global'
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Global />
+      <Story />
+    </>
+  ),
+]
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
