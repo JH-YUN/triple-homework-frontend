86a50a0d065e93854a9514d41fd6d5bef98e9627
junhan <wnsgks789@gmail.com>
2022-06-26 20:53:01 +0900

    chore: lint 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,26 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
-module.exports = createConfig({ type: 'frontend', project: './tsconfig.json' })
+const { extends: extendConfigs, overrides } = createConfig({
+  type: 'frontend',
+  project: './tsconfig.json',
+})
+
+module.exports = {
+  extends: [
+    ...extendConfigs,
+    // 확장할 규칙 이름...
+  ],
+  overrides: [
+    ...overrides,
+    {
+      files: ['**/*.stories.*'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+      },
+    },
+  ],
+  rules: {
+    // 개별 규칙
+  },
+  // plugins: ['@titicaca/eslint-config-triple/eslint-plugin-prettier'],
+}
