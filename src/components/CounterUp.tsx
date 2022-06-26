2e6c054be5a56f955642417abb9eea16638f3db4
junhan <wnsgks789@gmail.com>
2022-06-26 18:29:29 +0900

    feat: CounterUp 컴포넌트 기능 변경
    수식어와 분리, 카운트업의 기능만 가지고있도록

--- src/components/CounterUp.tsx
+++ src/components/CounterUp.tsx
@@ -2,24 +2,12 @@ import styled from 'styled-components'
 
 export interface CounterUpProps {
   counter: number
-  boldDesc?: string
-  desc?: string
 }
 
-const CounterLayout = styled.div`
-  font-size: 36px;
-  letter-spacing: -1px;
-  margin-bottom: 20px;
-  color: rgb(58, 58, 58);
-`
-export const CounterUp = ({ counter, boldDesc, desc }: CounterUpProps) => {
+export const CounterUp = ({ counter }: CounterUpProps) => {
   return (
-    <CounterLayout>
-      <strong>
-        {counter}
-        {boldDesc}
-      </strong>
-      {desc}
-    </CounterLayout>
+    <>
+      {counter}
+    </>
   )
 }
