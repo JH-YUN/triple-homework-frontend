20875dec18f409e129ea71663f888f53bc09528d
junhan <wnsgks789@gmail.com>
2022-06-29 17:25:55 +0900

    chore: strict mode 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
@@ -6,8 +5,8 @@ import Global from './style/global'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
+  <>
     <App />
     <Global />
-  </React.StrictMode>,
+  </>,
 )
